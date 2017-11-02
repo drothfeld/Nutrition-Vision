a24f7fcd25d4eade439b88bc6e3a1a0c4e3ea6c5
Dylan Rothfeld <drothfel@gmail.com>
2017-11-02 14:35:28 -0700

    initial commit and hiding status bar

---
+++ Nutrition Vision/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Nutrition Vision
+//
+//  Created by Dylan Rothfeld on 11/2/17.
+//  Copyright © 2017 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+
+}
+
