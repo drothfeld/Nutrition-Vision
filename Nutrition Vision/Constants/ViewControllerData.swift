6deb4897690164ee74123266a8b9f022221a8cab
Dylan Rothfeld <drothfel@gmail.com>
2017-11-09 12:53:17 -0800

    finished defining unhealthy ingredients and created list for UI display

---
+++ Nutrition Vision/Constants/ViewControllerData.swift
@@ -0,0 +1,62 @@
+//
+//  ViewControllerData.swift
+//  Nutrition Vision
+//
+//  Created by Dylan Rothfeld on 11/9/17.
+//  Copyright © 2017 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+let unhealthyIngredients: [Ingredient] = [
+    acesulfame_potassium,
+    agave_nectar,
+    aluminum,
+    annatto,
+    aspartame,
+    azodicarbonamide,
+    bha_bht,
+    butane,
+    brominated_vegetable_oil,
+    bleached_starch,
+    blue_one,
+    blue_two,
+    brown_ht,
+    bixin,
+    butane,
+    carrageenan,
+    camauba_wax,
+    chlorine_dioxide,
+    citrus_red_one,
+    citrus_red_two,
+    caramel_coloring,
+    disodium_inosinate,
+    disodium_guanylate,
+    enriched_flour,
+    green_three,
+    high_fructose_corn_syrup,
+    monosodium_glutamate,
+    magnesium_sulphate,
+    norbixin,
+    orange_b,
+    olestra,
+    potassium_bromate,
+    propyl_gallate,
+    propylene_glycol,
+    polysorbate_sixty,
+    paraben,
+    recombinant_bovine_growth_hormone,
+    refined_vegetable_oil,
+    red_fourty,
+    red_two,
+    red_three,
+    sodium_nitrate,
+    sulfites,
+    sodium_benzoate,
+    sodium_carboxymethyl_cellulose,
+    saccharin,
+    sucralose,
+    tert_butylhydroquinone,
+    yellow_five,
+    yellow_six
+]
