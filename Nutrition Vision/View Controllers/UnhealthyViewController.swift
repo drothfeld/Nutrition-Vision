b3e8dcb09fc39a8749c04fa8aa44c0bf5f72c8d9
Dylan Rothfeld <drothfel@gmail.com>
2017-11-09 08:57:45 -0800

    unhealthy table view auto populating with data

--- Nutrition Vision/View Controllers/UnhealthyViewController.swift
+++ Nutrition Vision/View Controllers/UnhealthyViewController.swift
@@ -8,7 +8,22 @@
 
 import UIKit
 
-class UnhealthyViewController: UIViewController {
+class UnhealthyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let myarray = ["item1", "item2", "item3"]
+    
+    // Number of Rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myarray.count
+    }
+    
+    
+    // Cell Data
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath)
+        cell.textLabel?.text = myarray[indexPath.item]
+        return cell
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
