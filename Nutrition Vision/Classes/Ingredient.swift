45f497fe3b6114a45a8e64dbd9fc93e6fdf2191d
Dylan Rothfeld <drothfel@gmail.com>
2017-11-09 09:31:29 -0800

    added ingredient class and defined some unhealthy ingredients

---
+++ Nutrition Vision/Classes/Ingredient.swift
@@ -0,0 +1,21 @@
+//
+//  Ingredient.swift
+//  Nutrition Vision
+//
+//  Created by Dylan Rothfeld on 11/9/17.
+//  Copyright © 2017 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+class Ingredient {
+    // Fields
+    var name: String
+    var description: String
+    
+    // Constructor
+    init(name: String, description: String) {
+        self.name = name
+        self.description = description
+    }
+}
---
+++ Nutrition Vision/Constants/UnhealthyIngredients.swift
@@ -0,0 +1,17 @@
+//
+//  UnhealthyIngredients.swift
+//  Nutrition Vision
+//
+//  Created by Dylan Rothfeld on 11/9/17.
+//  Copyright © 2017 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+let acesulfame_potassium = Ingredient(name: "Acesulfame Potassium", description: "Used with other artificial sweeteners in diet sodas and ice cream. Linked to lung and breast tumors in rats.")
+let agave_nectar = Ingredient(name: "Agave Nectar", description: "Sweetener derived from a cactus. Contains high levels of fructose, which causes insulin resistance, liver disease and inflammation of body tissues.")
+let aluminum = Ingredient(name: "Aluminum", description: "A preservative in some packaged foods that can cause cancer.")
+let annatto = Ingredient(name: "Annatto", description: "Food coloring that can cause hyperactivity in children and asthma.")
+let aspartame = Ingredient(name: "Aspartame", description: "An excitotoxin and thought to be a carcinogen. Can cause dizziness, headaches, blurred vision and stomach problems.")
+let azodicarbonamide = Ingredient(name: "Azodicarbonamide", description: "Used in bagels and buns. Can cause asthma.")
+
