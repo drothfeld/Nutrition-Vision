07001dd41e56f7e120b7e51b9bd02c9bd98fd574
Dylan Rothfeld <drothfel@gmail.com>
2017-11-09 09:42:08 -0800

    added B unhealthy ingredients

--- Nutrition Vision/Constants/UnhealthyIngredients.swift
+++ Nutrition Vision/Constants/UnhealthyIngredients.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
+// A
 let acesulfame_potassium = Ingredient(name: "Acesulfame Potassium", description: "Used with other artificial sweeteners in diet sodas and ice cream. Linked to lung and breast tumors in rats.")
 let agave_nectar = Ingredient(name: "Agave Nectar", description: "Sweetener derived from a cactus. Contains high levels of fructose, which causes insulin resistance, liver disease and inflammation of body tissues.")
 let aluminum = Ingredient(name: "Aluminum", description: "A preservative in some packaged foods that can cause cancer.")
 let annatto = Ingredient(name: "Annatto", description: "Food coloring that can cause hyperactivity in children and asthma.")
 let aspartame = Ingredient(name: "Aspartame", description: "An excitotoxin and thought to be a carcinogen. Can cause dizziness, headaches, blurred vision and stomach problems.")
 let azodicarbonamide = Ingredient(name: "Azodicarbonamide", description: "Used in bagels and buns. Can cause asthma.")
+
+// B
+let bha_bht = Ingredient(name: "BHA/BHT", description: "A fat preservative, used in foods to extend shelf life. Linked to cancerous tumor growth.")
+let butane = Ingredient(name: "Butane", description: "Put in chicken nuggets to keep them tasting fresh. A known carcinogen.")
+let brominated_vegetable_oil = Ingredient(name: "Brominated Vegetable Oil", description: "Keeps flavor oils in soft drinks suspended. Bromate is a poison and can cause organ damage and birth defects. Not required to be listed on food labels.")
+let bleached_starch = Ingredient(name: "Bleached Starch", description: "Can be used in many dairy products. Thought to be related to asthma and skin irritations.")
+let blue_one = Ingredient(name: "Blue #1", description: "Used in bakery products, candy and soft drinks. Can damage chromosomes and lead to cancer.")
+let blue_two = Ingredient(name: "Blue #2", description: "Used in candy and pet food beverages. Can cause brain tumors.")
+let brown_ht = Ingredient(name: "Brown HT", description: "Used in many packaged foods. Can cause hyperactivity in children, asthma and cancer.")
+let bixin = Ingredient(name: "Bixin", description: "Food coloring that can cause hyperactivity in children and asthma.")
+
 
