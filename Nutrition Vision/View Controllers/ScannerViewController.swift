02cf6d179fb468b4cf40dec39dba8b4a1b04dd28
Dylan Rothfeld <drothfel@gmail.com>
2017-12-06 13:56:18 -0800

    AV foundation camera session setup

--- Nutrition Vision/View Controllers/ScannerViewController.swift
+++ Nutrition Vision/View Controllers/ScannerViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ScannerViewController: UIViewController {
+    
+    let captureSession = AVCaptureSession()
+    
+    var previewLayer: CALayer!
+    var captureDevice: AVCaptureDevice!
 
     // Defined Values
     var currentScannedLabel: ScannedLabel = ScannedLabel(scannedImage: "null")
@@ -19,10 +25,49 @@ class ScannerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         resetScanner()
+        prepareCamera()
     }
     
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    // Used to set up the camera
+    func prepareCamera() {
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices
+        captureDevice = availableDevices.first
+        beginSession()
+    }
+    
+    // Start camera session
+    func beginSession() {
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(captureDeviceInput)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        self.previewLayer = previewLayer
+        self.view.layer.addSublayer(self.previewLayer)
+        self.previewLayer.frame = self.view.layer.frame
+        
+        captureSession.startRunning()
+            
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.videoSettings = [((kCVPixelBufferPixelFormatTypeKey as NSString) as String): NSNumber(value: kCVPixelFormatType_32BGRA)]
+        
+        dataOutput.alwaysDiscardsLateVideoFrames = true
+        
+        if captureSession.canAddOutput(dataOutput) {
+            captureSession.addOutput(dataOutput)
+        }
+        
+        captureSession.commitConfiguration()
+        
     }
     
     // Resetting all scanner values
