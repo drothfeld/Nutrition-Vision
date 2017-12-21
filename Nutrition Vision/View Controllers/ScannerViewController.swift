0d435b7be927899b55a523e94899a9c8e3937098
Dylan Rothfeld <drothfel@gmail.com>
2017-12-21 10:07:12 -0800

    scale image uiimage extension to keep aspect ratio

--- Nutrition Vision/View Controllers/ScannerViewController.swift
+++ Nutrition Vision/View Controllers/ScannerViewController.swift
@@ -162,3 +162,26 @@ class ScannerViewController: UIViewController, AVCaptureVideoDataOutputSampleBuf
 
 }
 
+// UIImage extension
+extension UIImage {
+    func scaleImage(_ maxDimension: CGFloat) -> UIImage? {
+        
+        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+        
+        if size.width > size.height {
+            let scaleFactor = size.height / size.width
+            scaledSize.height = scaledSize.width * scaleFactor
+        } else {
+            let scaleFactor = size.width / size.height
+            scaledSize.width = scaledSize.height * scaleFactor
+        }
+        
+        UIGraphicsBeginImageContext(scaledSize)
+        draw(in: CGRect(origin: .zero, size: scaledSize))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaledImage
+    }
+}
+
